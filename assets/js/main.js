2a9a50a79f9d35bccc14b60a8e996530f64c6f12
Luna R <snowluna@protonmail.com>
2022-07-13 20:02:04 +0000

    add functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,28 +4,37 @@ const todaysDateEl = document.getElementById("currentDay");
 
 todaysDateEl.textContent = todaysDate;
 
-// Load schedule from storage
-var schedule = window.localStorage.getItem( "workDaySchedule" );
-if ( schedule !== null )
-{
-    
-}
-
+// An array of one hour time blocks
 class timeBlock 
 {
-   constructor( startHour )
+   constructor( startHour, stopHour )
    {
+      this.timeBlock = [];
       
+      for ( let i = startHour;   i < stopHour;   i++ )
+      {
+         this.timeBlock.push( { hour: i,   body: "" } );
+      }
+
+      return this.timeBlock;
    }
 }
 
+// Load schedule from storage or create a new schedule
+var schedule = window.localStorage.getItem( "workDaySchedule" );
+if ( schedule === null )
+{
+   schedule = new timeBlock( 9, 17 );
+   window.localStorage.setItem("workDaySchedule", JSON.stringify(schedule));
+}
+
 // create time block element 
-function addTimeBlock( hour )
+function addTimeBlock( s, index )
 {
    // Create the elements for our time block
    ContainerEl = document.createElement("section");
    hourContainerEl = document.createElement("div");
-   hourTextEl = document.createElement("p");
+   hourTextEl = document.createElement("span");
    timeBlockTextEl = document.createElement("textarea");
    saveButtonEl = document.createElement("button");
 
@@ -36,9 +45,19 @@ function addTimeBlock( hour )
    ContainerEl.appendChild(saveButtonEl);
 
    document.querySelector( ".container" ).appendChild(ContainerEl);
+
+   ContainerEl.setAttribute("data-index", JSON.stringify(index))
+   timeBlockTextEl.textContent = s[ index ].body;
+   hourTextEl.textContent = s[ index ].hour;
+   saveButtonEl.textContent = "Save";
 }
 
-for ( let h = 9;  i < 17;  i++ )
+function saveTimeBlock( index )
 {
-   addTimeBlock( h );
+   localStorage.setItem("workDaySchedule")
+}
+
+for ( let i = 0;  i < schedule.length;  i++ )
+{
+   addTimeBlock( schedule, i );
 }
